dfeeb676d75e921f44101f580d3b8e6090212c9c
Shinwoo Kim <sk274@cornell.edu>
2018-09-28 10:36:37 -0400

    Separate message processing to message processor module

--- lib/archival_storage_ingest.rb
+++ lib/archival_storage_ingest.rb
@@ -2,10 +2,13 @@ require 'archival_storage_ingest/version'
 require 'archival_storage_ingest/messages/ingest_message'
 require 'archival_storage_ingest/messages/poller'
 require 'archival_storage_ingest/messages/queuer'
+require 'archival_storage_ingest/messages/message_processor'
 require 'archival_storage_ingest/workers/worker_pool'
 require 'archival_storage_ingest/workers/fixity_compare_worker'
 require 'archival_storage_ingest/workers/fixity_worker'
 require 'archival_storage_ingest/workers/transfer_worker'
+require 'yaml'
+require 'aws-sdk-sqs'
 
 module ArchivalStorageIngest
   # Your code goes here...
@@ -23,39 +26,58 @@ module ArchivalStorageIngest
         raise "Configuration file #{config_file} does not exist!"
       end
 
-      @config = config_file
-      @poller = Poller::SQSPoller.new
+      @config = YAML.load_file(config_file)
       @queuer = Queuer::SQSQueuer.new
-      @worker_pool = WorkerPool::CWorkerPool.new
     end
 
     def queue_ingest(ingest_config)
-      msg = IngestMessage::SQSMessage.new
-      @queuer.put_message(msg)
+      msg = IngestMessage::SQSMessage.new({
+        ingest_id: 'test_ingest_id_1',
+        type: IngestMessage::TYPE_TRANSFER_S3
+      })
+      @queuer.put_message("cular_development_ingest", msg)
     end
 
-    def start
-      while true
-        do_work
-        sleep(30) # get this value from config
+    def server
+      initialize_server()
+#      while true
+        do_work()
+        #sleep(30) # get this value from config
+#      end
+      p "server called!"
+    end
+
+    def initialize_server
+      sqs = Aws::SQS::Client.new
+      subscribed_queues = {}
+      puts @config.inspect
+      @config['subscribed_queues'].each do | queue_name |
+        queue_url = sqs.get_queue_url(queue_name: queue_name).queue_url
+        subscribed_queues[queue_name] = queue_url
       end
+
+      @poller = Poller::SQSPoller.new(subscribed_queues)
+      @worker_pool = WorkerPool::CWorkerPool.new
+      @message_processor = MessageProcessor::SQSMessageProcessor.new(@queuer)
     end
 
     def do_work
-      process_finished_job()
+      #process_finished_job()
 
       if !@worker_pool.is_available?
         # do nothing
         return
       end
 
-      msg = @poller.get_message
-      if msg.is_nil?
+      msg = @poller.get_message()
+      if msg.nil?
         # do nothing
+        puts "No message received"
         return
       end
 
-      process_message(msg)
+      
+      @message_processor.process_message(msg)
     end
 
     def process_finished_job
@@ -70,18 +92,6 @@ module ArchivalStorageIngest
         end
       end
       @worker_pool.clear_inactive_jobs
-    end
-
-    def process_message(msg)
-      case msg.type
-      when IngestMessage.TYPE_TRANSFER_S3
-      when IngestMessage.TYPE_TRANSFER_SFS
-      when IngestMessage.TYPE_FIXITY_S3
-      when IngestMessage.TYPE_FIXITY_SFS
-      when IngestMessage.TYPE_FIXITY_COMPARE
-      else
-        warn "Unknown message type: " + msg.type
-      end
     end
   end
 end

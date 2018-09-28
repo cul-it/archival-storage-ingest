9be9e10638dadd270c55ac17bf811121a26a7349
Shinwoo Kim <sk274@cornell.edu>
2018-09-28 14:42:30 -0400

    Add known queue names

---
+++ lib/archival_storage_ingest/messages/queues.rb
@@ -0,0 +1,9 @@
+# constants for queues
+module Queues
+  QUEUE_INGEST         = 'cular_development_ingest'.freeze
+  QUEUE_TRANSFER_S3    = 'cular_development_transfer_s3'.freeze
+  QUEUE_TRANSFER_SFS   = 'cular_development_transfer_sfs'.freeze
+  QUEUE_FIXITY_S3      = 'cular_development_fixity_s3'.freeze
+  QUEUE_FIXITY_SFS     = 'cular_development_fixity_sfs'.freeze
+  QUEUE_FIXITY_COMPARE = 'cular_development_comparison'.freeze
+end

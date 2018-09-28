29b303bf0a91635ffb04fec3dec595a60da1351f
Shinwoo Kim <sk274@cornell.edu>
2018-09-28 17:05:19 -0400

    Add debug statements

--- lib/archival_storage_ingest/messages/message_processor.rb
+++ lib/archival_storage_ingest/messages/message_processor.rb
@@ -1,6 +1,8 @@
 require 'archival_storage_ingest/messages/ingest_message'
 
+# Message processor implementation
 module MessageProcessor
+  # SQS message processor
   class SQSMessageProcessor
     def initialize(queuer)
       @queuer = queuer
@@ -8,19 +10,22 @@ module MessageProcessor
 
     def process_message(msg)
       case msg.type
+      when IngestMessage::TYPE_INGEST
+        puts 'ingest_message.rb:14 Message ' + IngestMessage::TYPE_INGEST + ' received!'
       when IngestMessage::TYPE_TRANSFER_S3
-        puts "Message " + IngestMessage::TYPE_TRANSFER_S3  + " received!"
+        puts 'ingest_message.rb:16 Message ' + IngestMessage::TYPE_TRANSFER_S3 + ' received!'
       when IngestMessage::TYPE_TRANSFER_SFS
-        puts "Message " + IngestMessage::TYPE_TRANSFER_SFS  + " received!"
+        puts 'ingest_message.rb:18 Message ' + IngestMessage::TYPE_TRANSFER_SFS + ' received!'
       when IngestMessage::TYPE_FIXITY_S3
-        puts "Message " + IngestMessage::TYPE_FIXITY_S3  + " received!"
+        puts 'ingest_message.rb:20 Message ' + IngestMessage::TYPE_FIXITY_S3 + ' received!'
       when IngestMessage::TYPE_FIXITY_SFS
-        puts "Message " + IngestMessage::TYPE_FIXITY_SFS  + " received!"
+        puts 'ingest_message.rb:22 Message ' + IngestMessage::TYPE_FIXITY_SFS + ' received!'
       when IngestMessage::TYPE_FIXITY_COMPARE
-        puts "Message " + IngestMessage::TYPE_FIXITY_COMPARE  + " received!"
+        puts 'ingest_message.rb:24 Message ' + IngestMessage::TYPE_FIXITY_COMPARE + ' received!'
       else
-        warn "Unknown message type: " + msg.type
+        warn 'Unknown message type: ' + msg.type
       end
+      puts 'ingest_message.rb:28 ' + msg.inspect
     end
   end
 end

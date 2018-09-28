0cc9aa6af88ffadaddc659a8351a71559a2193fb
Shinwoo Kim <sk274@cornell.edu>
2018-09-28 10:39:35 -0400

    Make queuer cache query url if used before and make put queue request

--- lib/archival_storage_ingest/messages/queuer.rb
+++ lib/archival_storage_ingest/messages/queuer.rb
@@ -1,6 +1,28 @@
+require 'aws-sdk-sqs'
+
 module Queuer
   class SQSQueuer
-    def put_message(msg)
+    def initialize()
+      @known_queues = {}
+    end
+
+    def put_message(queue_name, msg)
+      json_msg = msg.to_json
+      sqs = Aws::SQS::Client.new
+      if @known_queues[queue_name].nil?
+        queue_url = sqs.get_queue_url(queue_name: queue_name).queue_url
+        @known_queues[queue_name] = queue_url
+      end
+      return sqs.send_message({
+        queue_url: @known_queues[queue_name],
+        message_body: json_msg,
+        message_attributes: {
+          "ingest_id" => {
+            string_value: msg.ingest_id,
+            data_type: "String"
+          }
+        }
+      })
     end
   end
 end

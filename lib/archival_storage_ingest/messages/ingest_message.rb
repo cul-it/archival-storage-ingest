de52278edccda65d68b395dcd6217845a68e9666
Shinwoo Kim <sk274@cornell.edu>
2018-09-28 10:37:13 -0400

    Add basic functionality to convert to and from JSON

--- lib/archival_storage_ingest/messages/ingest_message.rb
+++ lib/archival_storage_ingest/messages/ingest_message.rb
@@ -1,4 +1,33 @@
+require 'json'
+
 module IngestMessage
+  TYPE_TRANSFER_S3    = 'Transfer S3'
+  TYPE_TRANSFER_SFS   = 'Transfer SFS'
+  TYPE_FIXITY_S3      = 'Fixity S3'
+  TYPE_FIXITY_SFS     = 'Fixity SFS'
+  TYPE_FIXITY_COMPARE = 'Fixity Compare'
+
+  def to_sqs_message(json)
+    return SQSMessage.new({
+      ingest_id: json[:ingest_id],
+      type: json[:type]
+    })
+  end
+
   class SQSMessage
+    def initialize(params)
+      @ingest_id = params[:ingest_id]
+      @type      = params[:type]
+    end
+
+    attr_reader :ingest_id
+    attr_accessor :type
+
+    def to_json
+      return JSON.generate({
+        :ingest_id => @ingest_id,
+        :type => @type
+      })
+    end
   end
 end

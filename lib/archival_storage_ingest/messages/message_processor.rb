3501e7ade27ae4538a21b8dd1bd0cf11beee2b52
Buddha Buck <blaisepascal@gmail.com>
2018-10-03 16:20:41 -0400

    Add transfer message processing.

--- lib/archival_storage_ingest/messages/message_processor.rb
+++ lib/archival_storage_ingest/messages/message_processor.rb
@@ -17,10 +17,9 @@ module MessageProcessor
       when IngestMessage::TYPE_INGEST
         process_ingest(msg)
       when IngestMessage::TYPE_TRANSFER_S3
-        @logger.info('Invoke transfer S3 worker')
-
+        process_transfer_s3(msg)
       when IngestMessage::TYPE_TRANSFER_SFS
-        @logger.info('Invoke transfer SFS worker')
+        process_transfer_sfs(msg)
       when IngestMessage::TYPE_FIXITY_S3
         @logger.info('Invoke fixity s3 worker')
       when IngestMessage::TYPE_FIXITY_SFS
@@ -31,14 +30,26 @@ module MessageProcessor
         @logger.info('Invalid message received, doing nothing')
       end
     rescue StandardError => e # TODO: flesh out error handling
-      puts e.message
-      puts e.backtrace.inspect
+      @logger.error("Could not process message ID:#{msg.ingest_id}")
+      @logger.error(msg.to_json)
+      @logger.error(e.message)
+      @logger.error(e.backtrace.inspect)
       @queuer.put_message(Queues::QUEUE_ERROR, msg)
     else # TODO: figure out how to delete message.
       # @queuer.delete_message(msg.queue, msg)
     end
 
     private
+
+    def process_transfer_sfs(msg)
+      @logger.info('Invoke transfer SFS worker')
+      send_message(msg, IngestMessage::TYPE_FIXITY_SFS)
+    end
+
+    def process_transfer_s3(msg)
+      @logger.info('Invoke transfer S3 worker')
+      send_message(msg, IngestMessage::TYPE_FIXITY_S3)
+    end
 
     def process_ingest(msg)
       @logger.info('Put transfer s3 and sfs messages')
@@ -50,7 +61,7 @@ module MessageProcessor
 
     def send_message(msg, type)
       msg_s3 = IngestMessage::SQSMessage.new(ingest_id: msg.ingest_id, type: type)
-      @queuer.put_message(Queues::TYPE2QUEUE[type], msg_s3)
+      @queuer.put_message((IngestMessage::queue_name_from_work_type type), msg_s3)
     end
   end
 end
--- lib/archival_storage_ingest/messages/queues.rb
+++ lib/archival_storage_ingest/messages/queues.rb
@@ -1,4 +1,5 @@
 # constants for queues
+
 module Queues
   QUEUE_INGEST         = 'cular_development_ingest'.freeze
   QUEUE_TRANSFER_S3    = 'cular_development_transfer_s3'.freeze
@@ -8,12 +9,4 @@ module Queues
   QUEUE_FIXITY_COMPARE = 'cular_development_comparison'.freeze
   QUEUE_ERROR = 'cular_development_error'.freeze
 
-  TYPE2QUEUE = {
-      IngestMessage::TYPE_INGEST => Queues::QUEUE_INGEST,
-      IngestMessage::TYPE_TRANSFER_S3 => Queues::QUEUE_TRANSFER_S3,
-      IngestMessage::TYPE_TRANSFER_SFS => Queues::QUEUE_TRANSFER_SFS,
-      IngestMessage::TYPE_FIXITY_S3 => Queues::QUEUE_FIXITY_S3,
-      IngestMessage::TYPE_FIXITY_SFS => Queues::QUEUE_FIXITY_SFS,
-      IngestMessage::TYPE_FIXITY_COMPARE => Queues::QUEUE_FIXITY_COMPARE
-  }.freeze
 end

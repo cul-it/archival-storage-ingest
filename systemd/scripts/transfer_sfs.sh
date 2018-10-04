1efe1df85b5e67f90f539600980923ae1378221b
Shinwoo Kim <sk274@cornell.edu>
2018-10-04 10:36:20 -0400

    Add systemd related files to run ingest server as daemon

---
+++ systemd/scripts/transfer_sfs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export archival_storage_ingest_config=/cul/app/archival_storage_ingest/archival-storage-ingest/systemd/conf/transfer_sfs.yaml
+
+export PATH="$PATH:/cul/app/archival_storage_ingest/rvm"
+source /cul/app/archival_storage_ingest/rvm/scripts/rvm
+rvm gemset use archival_storage_ingest_gemset
+archival_storage_ingest_server

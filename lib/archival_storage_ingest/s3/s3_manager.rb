172d4f80c3fab7f32e5dc7801e15f690ff40ab75
Shinwoo Kim <sk274@cornell.edu>
2018-10-26 15:25:16 -0400

    Remove test code

--- lib/archival_storage_ingest/s3/s3_manager.rb
+++ lib/archival_storage_ingest/s3/s3_manager.rb
@@ -5,25 +5,21 @@ require 'aws-sdk-s3'
 class S3Manager
   MAX_RETRY = 3
 
-  def initialize(max_retry = :MAX_RETRY)
+  def initialize(s3_bucket, max_retry = :MAX_RETRY)
+    @s3_bucket = s3_bucket
     @s3 = Aws::S3::Resource.new
     @max_retry = max_retry
   end
 
-  def upload_file(s3_bucket, s3_key, file_to_upload)
-    puts "upload file #{s3_bucket}, #{s3_key}, #{file_to_upload}"
-  end
-
-  def _upload_file(s3_bucket, s3_key, file_to_upload)
+  def upload_file(s3_key, file_to_upload)
     retries ||= 0
-    status = @s3.bucket(s3_bucket).object(s3_key).upload_file(file)
+    status = @s3.bucket(@s3_bucket).object(s3_key).upload_file(file_to_upload)
     raise Aws::S3::MultipartUploadError.new('Failed to upload file', 'Upload failed') unless status
+
+    status
+    true
   rescue Aws::S3::MultipartUploadError
     retry if (retries += 1) < @max_retry
     raise IngestException, "S3 upload failures reached max retry (#{MAX_RETRY}) for #{file_to_upload}"
   end
-
-  def retrieve_file(s3_bucket, s3_key) end
-
-  def calculate_checksum(s3_bucket, s3_key) end
 end

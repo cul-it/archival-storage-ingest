37e4ddf006133e90809a914469301becb5364e25
Shinwoo Kim <sk274@cornell.edu>
2018-10-30 11:49:32 -0400

    Add s3 transfer in progress queue

--- lib/archival_storage_ingest/messages/queues.rb
+++ lib/archival_storage_ingest/messages/queues.rb
@@ -3,11 +3,12 @@
 # constants for queues
 
 module Queues
-  QUEUE_INGEST = 'cular_development_ingest'
-  QUEUE_TRANSFER_S3 = 'cular_development_transfer_s3'
-  QUEUE_TRANSFER_SFS = 'cular_development_transfer_sfs'
-  QUEUE_FIXITY_S3 = 'cular_development_fixity_s3'
-  QUEUE_FIXITY_SFS = 'cular_development_fixity_sfs'
-  QUEUE_FIXITY_COMPARE = 'cular_development_comparison'
-  QUEUE_ERROR = 'cular_development_error'
+  QUEUE_INGEST = 'cular_development_ingest'.freeze
+  QUEUE_TRANSFER_S3 = 'cular_development_transfer_s3'.freeze
+  QUEUE_TRANSFER_S3_IN_PROGRESS = 'cular_development_transfer_s3'.freeze
+  QUEUE_TRANSFER_SFS = 'cular_development_transfer_sfs'.freeze
+  QUEUE_FIXITY_S3 = 'cular_development_fixity_s3'.freeze
+  QUEUE_FIXITY_SFS = 'cular_development_fixity_sfs'.freeze
+  QUEUE_FIXITY_COMPARE = 'cular_development_comparison'.freeze
+  QUEUE_ERROR = 'cular_development_error'.freeze
 end

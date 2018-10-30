d0df144c5a224e6a0b3a700c0c1d3eaa28ae7810
Buddha Buck <bb233@cornell.edu>
2018-10-30 14:25:57 -0400

    Change default error message to constant.

--- lib/archival_storage_ingest/exception/ingest_exception.rb
+++ lib/archival_storage_ingest/exception/ingest_exception.rb
@@ -2,7 +2,7 @@
 
 class IngestException < StandardError
   DEFAULT_ERROR_MESSAGE = 'Automated ingest encountered error'
-  def initialize(msg = :DEFAULT_ERROR_MESSAGE)
+  def initialize(msg = DEFAULT_ERROR_MESSAGE)
     super
   end
 end

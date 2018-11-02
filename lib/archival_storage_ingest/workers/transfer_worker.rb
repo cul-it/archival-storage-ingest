6f75e2da5d32e0f4117dbfde3ceee54f799e4b85
Shinwoo Kim <sk274@cornell.edu>
2018-11-02 11:24:50 -0400

    Refactor transfer workers

--- lib/archival_storage_ingest/workers/transfer_worker.rb
+++ lib/archival_storage_ingest/workers/transfer_worker.rb
@@ -16,61 +16,56 @@ module TransferWorker
   # https://stackoverflow.com/questions/357754/can-i-traverse-symlinked-directories-in-ruby-with-a-glob
   # I was able to follow symlink with Dir.glob('**/*/**')
   # As was mentioned in the link above, it DOES NOT give you the immediate children (dir or file).
-  # I could not get the "fix" to work **{,/*/**}/*.
+  # I could not get the "fix" to work - **{,/*/**}/*.
   # If I use **{,/*/**}/*, I get files in non-symlink'ed directories twice.
   # I will process immediate children and then use **/*/**.
 
-  class BaseTransferWorker < Workers::Worker
-    def work(msg)
-      path_to_trim = Pathname.new(msg.data_path)
-
-      pre_process(msg, path_to_trim)
-
-      process_immediate_children(msg, path_to_trim)
-
-      process_rest(msg, path_to_trim)
-
-      true
-    end
-
-    # For sfs worker, this is where it will try to create root depositor / collection
-    # if it doesn't exist.
-    def pre_process(_msg, _path_to_trim) end
-
-    def process_immediate_children(msg, path_to_trim)
+  class DirectoryWalker
+    def process_immediate_children(msg)
       Dir.glob("#{msg.effective_data_path}/*").each do |path|
         next if EXCLUDE_FILE_LIST[File.basename(path)]
 
-        process_path(path, path_to_trim, msg)
+        yield(path)
       end
     end
 
-    def process_rest(msg, path_to_trim)
+    def process_rest(msg)
       Dir.glob("#{msg.effective_data_path}/**/*/**").each do |path|
         next if EXCLUDE_FILE_LIST[File.basename(path)]
 
-        process_path(path, path_to_trim, msg)
+        yield(path)
       end
     end
-
-    # For s3 worker, only upload file.
-    # For sfs worker, create directory if doesn't exist or copy file.
-    def process_path(_path, _path_to_trim, _msg) end
   end
 
-  class S3Transferer < BaseTransferWorker
+  class S3Transferer < Workers::Worker
     # Pass s3_manager only for tests.
     def initialize(s3_manager = nil)
       @s3_manager = s3_manager || ArchivalStorageIngest.configuration.s3_manager
     end
 
-    # s3 worker doesn't use msg argument
+    def work(msg)
+      directory_walker = DirectoryWalker.new
+
+      path_to_trim = Pathname.new(msg.data_path)
+
+      directory_walker.process_immediate_children(msg) do |path|
+        process_path(path, path_to_trim)
+      end
+
+      directory_walker.process_rest(msg) do |path|
+        process_path(path, path_to_trim)
+      end
+
+      true
+    end
+
     # skip directory
     # upload file
-    def process_path(path, path_to_trim, _msg)
+    def process_path(path, path_to_trim)
       return if File.directory?(path)
 
-      s3_key = get_s3_key(path, path_to_trim)
+      s3_key = s3_key(path, path_to_trim)
       @s3_manager.upload_file(s3_key, path)
     end
 
@@ -78,30 +73,44 @@ module TransferWorker
     # file - /a/b/c/resource.txt
     # path_to_trim - /a/b
     # s3 key - c/resource.txt
-    def get_s3_key(file, path_to_trim)
+    def s3_key(file, path_to_trim)
       Pathname.new(file).relative_path_from(path_to_trim).to_s
-    end
-
-    def upload_file(s3_key, file_to_upload)
-      @s3_manager.upload_file(s3_key, file_to_upload)
     end
   end
 
-  class SFSTransferer < BaseTransferWorker
-    def pre_process(msg, path_to_trim)
-      deposit_root = get_dest_path(msg.dest_path, msg.effective_data_path, path_to_trim)
+  class SFSTransferer < Workers::Worker
+    def work(msg)
+      directory_walker = DirectoryWalker.new
+
+      path_to_trim = Pathname.new(msg.data_path)
+
+      create_collection_dir(msg, path_to_trim)
+
+      directory_walker.process_immediate_children(msg) do |path|
+        process_path(path, path_to_trim, msg.dest_path)
+      end
+
+      directory_walker.process_rest(msg) do |path|
+        process_path(path, path_to_trim, msg.dest_path)
+      end
+
+      true
+    end
+
+    def create_collection_dir(msg, path_to_trim)
+      deposit_root = generate_dest_path(msg.dest_path, msg.effective_data_path, path_to_trim)
       FileUtils.mkdir_p(deposit_root) unless File.exist?(deposit_root)
     end
 
     # create directory if doesn't exist
     # copy file
-    def process_path(path, path_to_trim, msg)
-      dest = get_dest_path(msg.dest_path, path, path_to_trim)
+    def process_path(path, path_to_trim, dest_root)
+      dest = generate_dest_path(dest_root, path, path_to_trim)
 
       if File.directory?(path)
-        FileUtils.mkdir dest unless File.exist? dest
+        FileUtils.mkdir(dest) unless File.exist?(dest)
       else
-        FileUtils.copy path, dest
+        FileUtils.copy(path, dest)
       end
     end
 
@@ -110,7 +119,7 @@ module TransferWorker
     # path - /b/c/d/resource.txt
     # path_to_trim - /b/c
     # return - /a/d/resource.txt
-    def get_dest_path(dest_root, path, path_to_trim)
+    def generate_dest_path(dest_root, path, path_to_trim)
       File.join(dest_root, Pathname.new(path).relative_path_from(path_to_trim).to_s)
     end
   end

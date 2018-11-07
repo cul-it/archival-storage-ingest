47ffbe2d436d7805cbe155860f515ce15de8270b
Shinwoo Kim <sk274@cornell.edu>
2018-11-07 11:32:25 -0500

    Update the scripts to be able to use the new env variables

--- systemd/scripts/fixity_comparison.sh
+++ systemd/scripts/fixity_comparison.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-export archival_storage_ingest_config=/cul/app/archival_storage_ingest/conf/fixity_comparison.yaml
+# export asi_compare_fixity_log_path='/cul/app/archival_storage_ingest/logs/compare_fixity.log'
+# export asi_compare_fixity_debug=false
+# export asi_compare_fixity_dry_run=false
+# export asi_compare_fixity_bucket=s3-cular
 
 export PATH="$PATH:/cul/app/archival_storage_ingest/rvm"
 source /cul/app/archival_storage_ingest/rvm/scripts/rvm
 rvm gemset use archival_storage_ingest
-archival_storage_ingest_server
+archival_storage_ingest_server_fixity_compare
--- systemd/scripts/transfer_sfs.sh
+++ systemd/scripts/transfer_sfs.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-export archival_storage_ingest_config=/cul/app/archival_storage_ingest/conf/transfer_sfs.yaml
+# export asi_sfs_transfer_log_path='/cul/app/archival_storage_ingest/logs/transfer_sfs.log'
+# export asi_sfs_transfer_debug=false
+# export asi_sfs_transfer_dry_run=false
+# export asi_sfs_transfer_bucket=s3-cular
 
 export PATH="$PATH:/cul/app/archival_storage_ingest/rvm"
 source /cul/app/archival_storage_ingest/rvm/scripts/rvm

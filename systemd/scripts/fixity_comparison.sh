d54f01efdcb1810521526a2867696f22baca401c
Shinwoo Kim <sk274@cornell.edu>
2018-11-28 11:01:45 -0500

    Add polling interval

--- systemd/scripts/fixity_comparison.sh
+++ systemd/scripts/fixity_comparison.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # export asi_compare_fixity_log_path='/cul/app/archival_storage_ingest/logs/compare_fixity.log'
-# export asi_compare_fixity_debug=false
-# export asi_compare_fixity_dry_run=false
-# export asi_compare_fixity_bucket=s3-cular
+# export asi_compare_fixity_debug=true
+# export asi_compare_fixity_dry_run=true
+# export asi_compare_fixity_bucket=s3-cular-dev
+# export asi_compare_fixity_polling_interval=60
 
 export PATH="$PATH:/cul/app/archival_storage_ingest/rvm"
 source /cul/app/archival_storage_ingest/rvm/scripts/rvm

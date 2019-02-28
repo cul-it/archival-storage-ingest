fd3e10474c320ec4f61ba8560fb623ee215a30c8
Shinwoo Kim <sk274@cornell.edu>
2019-02-28 10:54:41 -0500

    Refactor to use consistent naming scheme of service names

--- systemd/scripts/ingest_fixity_s3.sh
+++ systemd/scripts/ingest_fixity_s3.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-# export asi_s3_fixity_log_path='/cul/app/archival_storage_ingest/logs/fixity_s3.log'
-# export asi_s3_fixity_debug=true
-# export asi_s3_fixity_dry_run=true
-# export asi_s3_fixity_bucket=s3-cular-dev
-# export asi_s3_fixity_polling_interval=60
+# export asi_ingest_fixity_s3_log_path='/cul/app/archival_storage_ingest/logs/ingest_fixity_s3.log'
+# export asi_ingest_fixity_s3_debug=true
+# export asi_ingest_fixity_s3_dry_run=true
+# export asi_ingest_fixity_s3_bucket=s3-cular-dev
+# export asi_ingest_fixity_s3_polling_interval=60
+# export asi_ingest_fixity_s3_inhibit_file='/cul/app/archival_storage_ingest/control/ingest_fixity_s3.inhibit'
 
 export PATH="$PATH:/cul/app/archival_storage_ingest/rvm"
 source /cul/app/archival_storage_ingest/rvm/scripts/rvm

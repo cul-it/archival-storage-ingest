68cf82036c37e40e01361c528472a921c7705420
Shinwoo Kim <sk274@cornell.edu>
2019-04-26 15:55:21 -0400

    Add ok notification message to developers when dead letter monitor does not find any problem

--- spec/dead_letter_monitor_spec.rb
+++ spec/dead_letter_monitor_spec.rb
@@ -4,6 +4,8 @@ require 'rspec'
 require 'archival_storage_ingest/monitor/dead_letter_monitor'
 require 'archival_storage_ingest/messages/ingest_queue'
 require 'json'
+require 'mail'
+require 'mail/network/delivery_methods/test_mailer'
 
 RSpec.describe 'DeadLetterMonitor' do # rubocop:disable BlockLength
   let(:dead_letter_queue_name) { %w[dlq1 dlq2] }
@@ -17,25 +19,23 @@ RSpec.describe 'DeadLetterMonitor' do # rubocop:disable BlockLength
       ticket_id: 'CULAR-1937'
     }
   end
-  let(:dead_letter_message) { JSON.generate(dead_letter_message_hash) }
   let(:queuer) do
     sqs_queuer = spy('sqs_queuer')
     sqs_response = spy('sqs_response')
-    allow(sqs_response).to receive(:body) { dead_letter_message }
+    allow(sqs_response).to receive(:body) { JSON.generate(dead_letter_message_hash) }
     allow(sqs_queuer).to receive(:retrieve_single_message).with(dead_letter_queue_name[0]) { sqs_response }
     allow(sqs_queuer).to receive(:retrieve_single_message).with(dead_letter_queue_name[1]) { nil }
-
     sqs_queuer
   end
-  let(:ticket_handler) { spy('ticket_handler') }
 
-  context 'when dead letter message is found' do
-    it 'sends notification' do
+  context 'When dead letter message is found' do
+    it 'Adds error message to Jira ticket' do
+      ticket_handler = spy('ticket_handler')
       ArchivalStorageIngest.configure do |config|
         config.queuer = queuer
         config.ticket_handler = ticket_handler
       end
-      dead_letter_monitor = DeadLetterMonitor.new(dead_letter_queue_name)
+      dead_letter_monitor = DeadLetterMonitor.new(dead_letter_queues: dead_letter_queue_name)
 
       dead_letter_message = dead_letter_monitor.check_dead_letter_queue(dead_letter_queue_name[0])
       expect(dead_letter_message.ingest_id).to eq(dead_letter_message_hash[:ingest_id])
@@ -43,8 +43,37 @@ RSpec.describe 'DeadLetterMonitor' do # rubocop:disable BlockLength
       dead_letter_message = dead_letter_monitor.check_dead_letter_queue(dead_letter_queue_name[1])
       expect(dead_letter_message).to be_an_nil
 
+      # Instantiate this lazy variable so next defaults section actually takes effect!
+      Mail.defaults do
+        delivery_method :test
+      end
+      Mail::TestMailer.deliveries.clear
+
       dead_letter_monitor.check_dead_letter_queues
       expect(ticket_handler).to have_received(:update_issue_tracker).once
+      # no notify email sent when dead letter message is found
+      expect(Mail::TestMailer.deliveries.length).to eq(0)
+    end
+  end
+
+  context 'When no dead letter message if found' do
+    it 'Sends ok message to developers' do
+      ticket_handler = spy('ticket_handler')
+      ArchivalStorageIngest.configure do |config|
+        config.queuer = queuer
+        config.ticket_handler = ticket_handler
+      end
+      dead_letter_monitor = DeadLetterMonitor.new(dead_letter_queues: [dead_letter_queue_name[1]])
+
+      Mail.defaults do
+        delivery_method :test
+      end
+      Mail::TestMailer.deliveries.clear
+
+      dead_letter_monitor.check_dead_letter_queues
+      expect(ticket_handler).to have_received(:update_issue_tracker).exactly(0).times
+      # notify email sent when no dead letter message is found
+      expect(Mail::TestMailer.deliveries.length).to eq(2)
     end
   end
 end

f82c5549995609825c194c91d3d3f0481b8b5098
Shinwoo Kim <sk274@cornell.edu>
2019-07-17 15:57:24 -0400

    Add dead letter monitor shell script

---
+++ systemd/scripts/dead_letter_monitor.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# export asi_dead_letter_monitor_log_path='/cul/app/archival_storage_ingest/logs/dead_letter_monitor.log'
+# export asi_develop=true
+# export asi_dead_letter_monitor_develop=true
+
+export PATH="$PATH:/cul/app/archival_storage_ingest/rvm"
+source /cul/app/archival_storage_ingest/rvm/scripts/rvm
+rvm gemset use archival_storage_ingest
+dead_letter_monitor

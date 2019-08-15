877e645d23a75e4d22b09e6bebaa883ec3694673
Shinwoo Kim <sk274@cornell.edu>
2019-08-15 11:01:21 -0400

    Add support for source_path

--- spec/convert_xml_to_json_manifest_spec.rb
+++ spec/convert_xml_to_json_manifest_spec.rb
@@ -3,18 +3,21 @@
 require 'rspec'
 require 'archival_storage_ingest/manifests/convert_xml_to_json_manifest'
 
-RSpec.describe 'ConvertXmlToJsonMetadata' do
+RSpec.describe 'ConvertXmlToJsonMetadata' do # rubocop:disable BlockLength
   # rubocop:disable Metrics/LineLength
   let(:hashdeep_manifest_file) { File.join(File.dirname(__FILE__), 'resources', 'manifests', 'hashdeep_manifest.xml') }
   let(:collection_manifest_file) { File.join(File.dirname(__FILE__), 'resources', 'manifests', 'hashdeep_collection_manifest.json') }
   let(:ingest_manifest_file) { File.join(File.dirname(__FILE__), 'resources', 'manifests', 'hashdeep_ingest_manifest.json') }
   # rubocop:enable Metrics/LineLength
+  let(:source_path) { 'bogus_source_path' }
 
   context 'When hashdeep generated xml is passed' do
     it 'converts to manifest' do
       cxtjm = Manifests::ConvertXmlToJsonManifest.new
       collection_manifest = Manifests.read_manifest(filename: collection_manifest_file)
-      manifest_response = cxtjm.generate_ingest_manifest(xml: hashdeep_manifest_file, manifest: collection_manifest, depth: 2)
+      manifest_response = cxtjm.generate_ingest_manifest(xml: hashdeep_manifest_file,
+                                                         manifest: collection_manifest, depth: 2,
+                                                         source_path: source_path)
       ingest_manifest = manifest_response.ingest_manifest
       overwrite_list = manifest_response.overwrite_list
       expected_ingest_manifest = Manifests.read_manifest(filename: ingest_manifest_file)
@@ -27,6 +30,9 @@ RSpec.describe 'ConvertXmlToJsonMetadata' do
       expect(overwrite_list[expected_overwrite_filepath][:collection_file_entry].sha1).to eq('deadbeef11111111deadbeef11111111deadbeef')
       expect(overwrite_list[expected_overwrite_filepath][:ingest_file_entry].sha1).to eq('deadbeef55555555deadbeef55555555deadbeef')
       # rubocop:enable Metrics/LineLength
+      ingest_manifest.walk_packages do |package|
+        expect(package.source_path).to eq(source_path)
+      end
     end
   end
 end

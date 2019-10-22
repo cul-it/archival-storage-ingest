a7c547963a9fc5e0838d40e52bd479b2bd998074
Shinwoo Kim <sk274@cornell.edu>
2019-10-22 09:40:01 -0400

    Add manifest of manifests model

---
+++ lib/archival_storage_ingest/manifests/manifest_of_manifests.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'json'
+
+module Manifests
+  class ManifestOfManifests
+    attr_accessor :mom_path, :manifest_of_manifests
+
+    def initialize(manifests_path)
+      @mom_path = manifests_path
+      f = File.new(manifests_path, 'r')
+      mom_hash = JSON.parse(f.read, symbolize_names: true)
+      f.close
+      @manifest_of_manifests = []
+      mom_hash.each do |manifest_def_hash|
+        @manifest_of_manifests << ManifestDefinition.new(manifest_def_hash)
+      end
+    end
+
+    def manifest_definition(depositor:, collection:)
+      manifest_of_manifests.find { |i| i.depositor.eql?(depositor) && i.collection.eql?(collection) }
+    end
+  end
+
+  class ManifestDefinition
+    attr_accessor :depositor, :collection, :sha1, :sfs, :depcol, :path, :s3_key
+    def initialize(manifest_def_hash)
+      @depositor = manifest_def_hash[:depositor]
+      @collection = manifest_def_hash[:collection]
+      @sha1 = manifest_def_hash[:sha1]
+      @sfs = manifest_def_hash[:sfs]
+      @depcol = manifest_def_hash[:depcol]
+      @path = manifest_def_hash[:path]
+      @s3_key = manifest_def_hash[:s3Key]
+    end
+  end
+end

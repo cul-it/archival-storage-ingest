0d50585093ed1c8dc1a862879d36b38ea1577b67
Shinwoo Kim <sk274@cornell.edu>
2019-10-30 12:33:12 -0400

    Add type attribute to periodic fixity configuration file; refactor code

--- lib/archival_storage_ingest/preingest/ingest_env_initializer.rb
+++ lib/archival_storage_ingest/preingest/ingest_env_initializer.rb
@@ -5,40 +5,38 @@ require 'archival_storage_ingest/manifests/manifests'
 require 'archival_storage_ingest/manifests/manifest_merger'
 require 'archival_storage_ingest/manifests/manifest_missing_attribute_populator'
 require 'archival_storage_ingest/manifests/manifest_to_filesystem_comparator'
+require 'archival_storage_ingest/messages/ingest_message'
 require 'fileutils'
 require 'yaml'
 
 module Preingest
   DEFAULT_INGEST_ROOT = '/cul/app/archival_storage_ingest/ingest'
   DEFAULT_SFS_ROOT    = '/cul/data'
-  NONE_COLLECTION_MANIFEST = 'none'
+  NO_COLLECTION_MANIFEST = 'none'
 
   class IngestEnvInitializer
-    attr_accessor :ingest_root, :sfs_root, :depositor, :collection_id,
-                  :ingest_manifest, :collection_manifest, :ingest_config
+    attr_accessor :ingest_root, :sfs_root, :depositor, :collection_id, :collection_root
+
     def initialize(ingest_root: DEFAULT_INGEST_ROOT, sfs_root: DEFAULT_SFS_ROOT)
       @ingest_root   = ingest_root
       @sfs_root      = sfs_root
       @depositor     = nil
       @collection_id = nil
-      @ingest_manifest = nil
-      @collection_manifest = nil
-      @ingest_config = nil
     end
 
     def initialize_ingest_env(data:, cmf:, imf:, sfs_location:, ticket_id:)
       manifest = Manifests.read_manifest(filename: imf)
       @depositor = manifest.depositor
       @collection_id = manifest.collection_id
-      collection_root = File.join(@ingest_root, @depositor, @collection_id)
-      data_root = _initialize_data(collection_root: collection_root, data: data)
-      @ingest_manifest, @collection_manifest = _initialize_manifests(collection_root: collection_root,
-                                                                     cmf: cmf, imf: imf, data_root: data_root)
-      @ingest_config = _initialize_config(collection_root: collection_root, sfs_location: sfs_location,
-                                          ingest_manifest_path: ingest_manifest, ticket_id: ticket_id)
+      @collection_root = File.join(ingest_root, depositor, collection_id)
+      data_root = _initialize_data(data: data)
+      im_path, _cm_path = _initialize_manifests(cmf: cmf, imf: imf,
+                                                data_root: data_root)
+      _initialize_config(sfs_location: sfs_location,
+                         ingest_manifest_path: im_path, ticket_id: ticket_id)
     end
 
-    def _initialize_data(collection_root:, data:)
+    def _initialize_data(data:)
       data_root = File.join(collection_root, 'data')
       depositor_dir = File.join(data_root, depositor)
       FileUtils.mkdir_p(depositor_dir)
@@ -46,7 +44,7 @@ module Preingest
       File.join(depositor_dir, collection_id)
     end
 
-    def _initialize_manifests(collection_root:, cmf:, imf:, data_root:)
+    def _initialize_manifests(cmf:, imf:, data_root:)
       manifest_dir = File.join(collection_root, 'manifest')
 
       # ingest manifest
@@ -62,7 +60,7 @@ module Preingest
     end
 
     def setup_collection_manifest(manifest_dir:, im_path:, cmf:)
-      return if cmf.eql?(NONE_COLLECTION_MANIFEST)
+      return if cmf.eql?(NO_COLLECTION_MANIFEST)
 
       collection_manifest_dir = File.join(manifest_dir, 'collection_manifest')
       cm_path = _initialize_manifest(manifest_dir: collection_manifest_dir, manifest_file: cmf)
@@ -97,17 +95,36 @@ module Preingest
       File.open(collection_manifest, 'w') { |file| file.write(json_to_write) }
     end
 
-    def _initialize_config(collection_root:, sfs_location:, ingest_manifest_path:, ticket_id:)
-      config_dir = File.join(collection_root, 'config')
-      FileUtils.mkdir_p(config_dir)
-      dest_path = File.join(sfs_root, sfs_location, depositor, collection_id)
-      ingest_config = {
-        depositor: depositor, collection: collection_id, dest_path: dest_path,
-        ingest_manifest: ingest_manifest_path, ticket_id: ticket_id
-      }
-      ingest_config_file = File.join(config_dir, 'ingest_config.yaml')
+    def _initialize_config(sfs_location:, ingest_manifest_path:, ticket_id:)
+      ingest_config = generate_config(sfs_location: sfs_location, ingest_manifest_path:
+                                      ingest_manifest_path, ticket_id: ticket_id)
+      ingest_config_file = prepare_config_path
       File.open(ingest_config_file, 'w') { |file| file.write(ingest_config.to_yaml) }
-      ingest_config_file
+    end
+
+    def generate_config(sfs_location:, ingest_manifest_path:, ticket_id:)
+      dest_path = File.join(sfs_root, sfs_location, depositor, collection_id)
+      {
+        type: work_type,
+        depositor: depositor, collection: collection_id,
+        dest_path: dest_path, ingest_manifest: ingest_manifest_path,
+        ticket_id: ticket_id
+      }
+    end
+
+    def work_type
+      IngestMessage::TYPE_INGEST
+    end
+
+    def prepare_config_path
+      config = config_path
+      parent = File.dirname(config)
+      FileUtils.mkdir_p(parent)
+      config
+    end
+
+    def config_path
+      File.join(collection_root, 'config', 'ingest_config.yaml')
     end
   end
 end

738350eb5dc7f8f7360137a22b6583a7d52a9438
Robbie Douglas <rld244@cornell.edu>
2020-09-10 23:41:23 -0400

    cular-2761 Add archive_size.rb

---
+++ lib/misc/archive_size.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'json'
+
+module ArchiveSize
+    class ArchiveSize
+        def initialize(archives:)
+            @archives = archives
+        end
+        def archive_size
+            json_data = {}
+            json_data[:archives] = []
+            for a in @archives do
+                df = `df #{a[:path]}`
+                a[:size] = df.split[8]
+                a[:used] = df.split[9]
+                a[:available] = df.split[10]
+                (json_data[:archives]) << a
+            end
+            JSON.pretty_generate(json_data)
+        end
+    end
+end

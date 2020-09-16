5b8a3bf8b779372dc9542c17590d007ecc200e09
Robbie Douglas <rld244@aws-108-069.internal.library.cornell.edu>
2020-09-15 21:34:25 -0400

    Rubocop auto-corrected errors

--- lib/misc/archive_size.rb
+++ lib/misc/archive_size.rb
@@ -5,25 +5,27 @@
 require 'json'
 
 module ArchiveSize
-    class ArchiveSize
-        def initialize(archives:, s3_manager:)
-            @archives = archives
-            @s3_manager = s3_manager
-        end
-        def archive_size
-            json_data = {}
-            json_data[:archives] = []
-            for a in @archives do
-                df = `df #{a[:path]}`
-                a[:size] = df.split[8]
-                a[:used] = df.split[9]
-                a[:available] = df.split[10]
-                (json_data[:archives]) << a
-            end
-            JSON.pretty_generate(json_data)
-        end
-        def deploy_asif_archive_size
-          @s3_manager.upload_asif_manifest(s3_key: "cular_archive_space.json", file: archive_size)
-        end
+  class ArchiveSize
+    def initialize(archives:, s3_manager:)
+      @archives = archives
+      @s3_manager = s3_manager
     end
+
+    def archive_size
+      json_data = {}
+      json_data[:archives] = []
+      @archives.each do |a|
+        df = `df #{a[:path]}`
+        a[:size] = df.split[8]
+        a[:used] = df.split[9]
+        a[:available] = df.split[10]
+        (json_data[:archives]) << a
+      end
+      JSON.pretty_generate(json_data)
+    end
+
+    def deploy_asif_archive_size
+      @s3_manager.upload_asif_manifest(s3_key: 'cular_archive_space.json', file: archive_size)
+    end
+  end
 end

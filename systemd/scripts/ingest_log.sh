a8cd4fa78722bfe392a09c9c541783d15366010a
Shinwoo Kim <sk274@cornell.edu>
2021-12-08 14:56:26 -0500

    Add logger systemd script and shell script

---
+++ systemd/scripts/ingest_log.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# export asi_s3_ingest_log_path='/cul/app/archival_storage_ingest/logs/ingest.log'
+# export asi_ingest_dry_run=true
+export asi_ingest_logger_polling_interval=5
+# export asi_ingest_develop=true
+# export asi_develop=true
+
+export PATH="$PATH:/cul/app/archival_storage_ingest/rvm"
+source /cul/app/archival_storage_ingest/rvm/scripts/rvm
+rvm gemset use archival_storage_ingest
+archival_storage_server_log

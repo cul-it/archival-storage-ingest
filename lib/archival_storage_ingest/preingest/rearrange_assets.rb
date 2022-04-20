8a772310e79045384a6af63ed1bcdb05578a5669
Shinwoo Kim <sk274@cornell.edu>
2022-04-20 11:46:59 -0400

    Update rearrange asset script for classic migration

--- lib/archival_storage_ingest/preingest/rearrange_assets.rb
+++ lib/archival_storage_ingest/preingest/rearrange_assets.rb
@@ -5,12 +5,11 @@ require 'csv'
 # filepath,size,sha1,JHOVE_timestamp,pid,ingest_date_calculated,to_documentation,migrated_name
 module Preingest
   class AssetRearranger
-    def source_path_key
-      'filepath'
-    end
+    attr_accessor :old_path_key, :new_path_key
 
-    def new_path_key
-      'migrated_name'
+    def initialize(old_path_key:, new_path_key:)
+      @old_path_key = old_path_key
+      @new_path_key = new_path_key
     end
 
     def rearrange_data_structure(depositor:, collection:, arrange_info_csv:, source_path:, staging_root:)
@@ -18,20 +17,19 @@ module Preingest
       arrange_info = populate_arrange_info(arrange_info_csv: arrange_info_csv)
 
       arrange_info.each_pair do |key, value|
-        rearrange_asset(source_path: File.join(source_path, key), staging_path: File.join(staging_path, value))
+        rearrange_asset(source_path: File.join(source_path, key),
+                        staging_path: File.join(staging_path, value))
       end
     end
 
     def populate_arrange_info(arrange_info_csv:)
       arrange_info = {}
       CSV.foreach(arrange_info_csv, headers: true) do |row|
-        next if skip_row(row: row)
-
-        arrange_info[row[source_path_key]] = if row[new_path_key] == 'SAME'
-                                               row[source_path_key]
-                                             else
-                                               row[new_path_key]
-                                             end
+        arrange_info[row[old_path_key]] = if row[new_path_key] == 'SAME'
+                                            row[old_path_key]
+                                          else
+                                            row[new_path_key]
+                                          end
       end
 
       arrange_info
@@ -41,10 +39,6 @@ module Preingest
       parent = File.dirname(staging_path)
       FileUtils.mkdir_p(parent)
       File.symlink(source_path, staging_path)
-    end
-
-    def skip_row(row:)
-      row['to_documentation'] == 'Y'
     end
   end
 end

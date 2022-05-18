7e4d76dc9e8292d07d02b94acb4f42bab475e21a
Shinwoo Kim <sk274@cornell.edu>
2022-05-18 11:17:30 -0400

    Make application logger get credentials from SSM; update queue names to conform with terraform

--- lib/archival_storage_ingest/logs/application_logger.rb
+++ lib/archival_storage_ingest/logs/application_logger.rb
@@ -4,9 +4,27 @@ require 'opensearch'
 require 'time'
 
 class ApplicationLogger
-  def initialize(opensearch_url:, index_name:)
-    @client = OpenSearch::Client.new url: opensearch_url, log: true
+  attr_writer :ssm_client
+
+  def s3
+    @ssm_client ||= Aws::SSM::Client.new
+  end
+
+  def initialize(opensearch_url: nil, stage:, index_name:)
+    opensearch_url = ssm_param("/cular/archivalstorage/#{stage}/opensearch/opensearch_url") unless opensearch_url
+    opensearch_main_user = ssm_param("/cular/archivalstorage/#{stage}/opensearch/opensearch_main_user")
+    opensearch_main_password = ssm_param("/cular/archivalstorage/#{stage}/opensearch/opensearch_main_password")
+
+    @client = OpenSearch::Client.new(
+      host: opensearch_url,
+      user: opensearch_main_user,
+      password: opensearch_main_password
+    )
     @index_name = index_name
+  end
+
+  def ssm_param(param)
+    @ssm_client.get_parameter({name: param})
   end
 
   def log(log_document)

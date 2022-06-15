fb50e6d32136bf77d55225888f08d9e626423ec3
Shinwoo Kim <sk274@cornell.edu>
2022-06-15 13:41:16 -0400

    Standardize executor initialization

--- lib/archival_storage_ingest/ingest_utils/configure_helper.rb
+++ lib/archival_storage_ingest/ingest_utils/configure_helper.rb
@@ -4,36 +4,37 @@ require 'archival_storage_ingest'
 
 module IngestUtils
   class ConfigureHelper
-    def configure(config:, params:)
-      config.stage = ArchivalStorageIngest::STAGE_PROD
-      config.s3_bucket = 's3-cular'
-      config.debug = ENV['asi_debug'] ? true : false
-      config.develop = false
+    attr_reader :stage, :s3_bucket, :debug, :develop, :message_queue_name, :in_progress_queue_name, :dest_queue_names
 
-      config = configure_stage(config: config, params: params)
+    def initialize(params)
+      @stage = ArchivalStorageIngest::STAGE_PROD
+      @stage = ArchivalStorageIngest::STAGE_DEV if params[:asi_develop]
+      @stage = ArchivalStorageIngest::STAGE_SANDBOX if params[:asi_sandbox]
 
-      configure_queues(config: config, params: params)
+      @s3_bucket = stage == ArchivalStorageIngest::STAGE_PROD ? 's3-cular' : "s3-cular-#{stage}"
+      @develop = stage != ArchivalStorageIngest::STAGE_PROD
+      @debug = stage != ArchivalStorageIngest::STAGE_PROD
+
+      configure_queues(params)
     end
 
-    def configure_stage(config:, params:)
-      return config unless params[:asi_develop] || params[:asi_sandbox]
-
-      config.debug = true
-      config.develop = true
-
-      config.stage = params[:asi_develop] ? ArchivalStorageIngest::STAGE_DEV : ArchivalStorageIngest::STAGE_SANDBOX
-      config.s3_bucket = "s3-cular-#{config.stage}"
-
-      config
-    end
-
-    def configure_queues(config:, params:)
-      config.message_queue_name = Queues.resolve_queue_name(queue: params[:queue_name], stage: config.stage)
-      config.message_queue_name = Queues.resolve_in_progress_queue_name(queue: params[:queue_name], stage: config.stage)
-      config.dest_queue_names = []
+    def configure_queues(params)
+      @message_queue_name = Queues.resolve_queue_name(stage: stage, queue: params[:queue_name])
+      @in_progress_queue_name = Queues.resolve_in_progress_queue_name(stage: stage, queue: params[:queue_name])
+      @dest_queue_names = []
       params[:dest_queue_names].each do |dest_queue_name|
-        config.dest_queue_names.append(Queues.resolve_queue_name(queue: dest_queue_name, stage: config.stage))
+        dest_queue_names.append(Queues.resolve_queue_name(queue: dest_queue_name, stage: stage))
       end
+    end
+
+    def configure(config)
+      config.stage = stage
+      config.message_queue_name = message_queue_name
+      config.in_progress_queue_name = in_progress_queue_name
+      config.dest_queue_names = dest_queue_names
+      config.develop = develop
+      config.debug = debug
+      config.s3_bucket = s3_bucket
 
       config
     end

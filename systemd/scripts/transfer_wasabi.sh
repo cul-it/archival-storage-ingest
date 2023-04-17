818ecc35da6a8c7044bf18ae58459ca782e030a8
Shinwoo Kim <sk274@cornell.edu>
2023-04-17 13:04:23 -0400

    Add Wasabi systemd service files

---
+++ systemd/scripts/transfer_wasabi.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# export asi_transfer_s3_log_path='/cul/app/archival_storage_ingest/logs/transfer_s3.log'
+# export asi_transfer_s3_dry_run=true
+# export asi_transfer_s3_polling_interval=60
+# export asi_transfer_s3_inhibit_file='/cul/app/archival_storage_ingest/control/transfer_s3.inhibit'
+# export asi_transfer_s3_develop=true
+# export asi_develop=true
+export use_lambda_logger=true
+export PATH="$PATH:/cul/app/archival_storage_ingest/rvm"
+source /cul/app/archival_storage_ingest/rvm/scripts/rvm
+rvm gemset use archival_storage_ingest
+archival_storage_server_transfer_wasabi
